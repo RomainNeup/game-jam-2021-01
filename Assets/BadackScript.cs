a3b867d62a5805411ebea50c76a6cadba75bee1d
Axelm21 <axel.medan@epitech.eu>
2021-01-30 13:01:10 +0100

    music bg and jump

---
+++ Assets/BadackScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadackScript : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col) {
+        SoundManagerScript.playsound();
+    }
+}
---
+++ Assets/JumpAudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAudioManager : MonoBehaviour
+{
+    public class AudioClip jumpsound;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        jumpsound = Resources.Load<AudioClip> ("jumpdbz");
+        audioSrc =  GetComponent <AudioSource> ();
+    }
+    public static viod playSound()
+    {
+        audioSrc.PlayOneShot(jumpsound);
+    }
+}
