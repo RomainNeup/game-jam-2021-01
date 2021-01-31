cf1c02068e002895452e9dff530be83a96e679ce
Thomas Strafrac <thomas.strafrac@epitech.eu>
2021-01-31 20:23:18 +0100

    game over manage

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GOPanel;
+
+    void Start()
+    {
+        GOPanel.SetActive(false);
+    }
+
+    void GameOver()
+    {
+        GOPanel.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
