87d7dea06c1084b0b365666670c3ab7a084449fd
Axelm21 <axel.medan@epitech.eu>
2021-01-31 21:20:55 +0100

    Update GameManager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,15 +6,37 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject GOPanel;
+    public GameObject StartPanel;
+    public GameObject GameOverlay;
 
     void Start()
     {
-        GOPanel.SetActive(false);
+        CleanScreen();
+        StartMenu();
     }
 
+    void CleanScreen()
+    {
+        GOPanel.SetActive(false);
+        StartPanel.SetActive(false);
+        GameOverlay.SetActive(false);
+    }
     void GameOver()
     {
+        CleanScreen();
         GOPanel.SetActive(true);
+    }
+
+    void StartMenu()
+    {
+        CleanScreen();
+        StartPanel.SetActive(true);
+    }
+
+    void Game()
+    {
+        CleanScreen();
+        GameOverlay.SetActive(true);
     }
 
     void Update()
